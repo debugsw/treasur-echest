ab0d565ac0d62871c74b59116efc8b2e27979499
liushuai <shuai012192@126.com>
2019-09-03 13:44:48 +0800

    多线程demo

---
+++ src/main/java/com/liushuai/treasurechest/multith/BatchUpdatePrdByParamsExecutor.java
@@ -0,0 +1,19 @@
+package com.liushuai.treasurechest.multith;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author: ls
+ * @modifier
+ * @Description:
+ * @Date: 2019/9/3 11:11
+ */
+@Slf4j
+public class BatchUpdatePrdByParamsExecutor implements Runnable{
+
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/com/liushuai/treasurechest/multith/MultithTest.java
@@ -0,0 +1,20 @@
+package com.liushuai.treasurechest.multith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+/**
+ * @Author: ls
+ * @modifier
+ * @Description:
+ * @Date: 2019/9/3 11:09
+ */
+public class MultithTest {
+
+    @Autowired
+    private ThreadPoolTaskExecutor taskExecutor;
+
+    public static void main(String[] args) {
+
+    }
+}
